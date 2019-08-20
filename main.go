3a5c6b3dc211271f826a15fec27f064ff48678a8
Richard Francoeur <Ricardo@RicardoFranco.local>
2019-08-19 21:35:19 -0400

    sample microservice which provide time

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type timeHandler struct {
+	format string
+}
+
+func (th *timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(th.format)
+	w.Write([]byte("The time is: " + tm))
+}
 
 func main() {
-    fmt.Println("hello world")
+	mux := http.NewServeMux()
+
+	th := &timeHandler{format: time.RFC1123}
+	mux.Handle("/", th)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":8000", mux)
 }
